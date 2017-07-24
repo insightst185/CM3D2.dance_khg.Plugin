0fc3a9dced1e10e23a63ddd68304f67303fa6966
insightst185 <insightfc3sst185@gmail.com>
2017-07-25 02:13:54 +0900

    0.0.0.2

--- CM3D2.dance_khg.cs
+++ CM3D2.dance_khg.cs
@@ -25,14 +25,10 @@ namespace CM3D2.dance_cm3d2_006_ssn
         private int[] presetPos = new int[MAX_LISTED_MAID];
         private Boolean maidSetting = false;
         private Boolean[] motionSetting = new Boolean[MAX_LISTED_MAID];
-//        private int setCount = 0;
-//        private float nextFire = 0.0f;
-//        private Maid maid0;
         private Maid maid;
         
         private void SetPreset(Maid maid, string fileName)
         {
-            //カスタムメイド3D したらば談話室 「カスタムメイド3D2」改造スレッド その8 >>11さんのコードからの引用
             var preset = GameMain.Instance.CharacterMgr.PresetLoad(Path.Combine(Path.GetFullPath(".\\") + "Preset", fileName));
             GameMain.Instance.CharacterMgr.PresetSet(maid, preset);
         }
@@ -55,7 +51,6 @@ namespace CM3D2.dance_cm3d2_006_ssn
             for(int i = 0; i < MAX_LISTED_MAID; i++){
                 motionSetting[i] = false;
             }
-//            setCount = 0;
         }
 
         private void Update()
@@ -66,16 +61,6 @@ namespace CM3D2.dance_cm3d2_006_ssn
             }
             if (!Enum.IsDefined(typeof(TargetLevel), level)) return;
             if(maidSetting == true) return;
-//            if(maidSetting == true){
-//               if(nextFire == 0.0f || nextFire > Time.time) return;
-//               for(int i = 0; i < MAX_LISTED_MAID; i++){
-//                   if (motionSetting[i] == true){
-//                       maid = GameMain.Instance.CharacterMgr.GetMaid(i + 1);
-//                       maid.body0.m_Bones.GetComponent<Animation>()["dance_cm3d2_005_khg_f.anm"].time = maid0.body0.m_Bones.GetComponent<Animation>()["dance_cm3d2_005_khg_f.anm"].time;
-//                   }
-//               }
-//               nextFire = 0.0f;
-//            }
             for(int i = 0; i < MAX_LISTED_MAID; i++){
                 if(xmlManager.listPreset[i] != null){
                    maid = GameMain.Instance.CharacterMgr.GetMaid(i + 1);
@@ -92,9 +77,10 @@ namespace CM3D2.dance_cm3d2_006_ssn
         }
         
         private void LateUpdate(){
+
             if (!Enum.IsDefined(typeof(TargetLevel), level)) return;
+            GameMain.Instance.MainCamera.GetComponent<Camera>().fieldOfView = xmlManager.fov;
             if(maidSetting == false) return;
-//            if(nextFire != 0.0f) return;
             for(int i = 0; i < MAX_LISTED_MAID; i++){
                 if (motionSetting[i] == false){
                     if(xmlManager.listPreset[i] != null){
@@ -104,25 +90,17 @@ namespace CM3D2.dance_cm3d2_006_ssn
                             int m_MotionLoad = (int)field.GetValue(maid);
 
                             if(m_MotionLoad == 0){
-//                                maid0 = GameMain.Instance.CharacterMgr.GetMaid(0);
                                 maid.CrossFade("dance_cm3d2_005_khg_f.anm", false, false, false, 0.0f, 1f);
-//                                maid.body0.m_Bones.GetComponent<Animation>()["dance_cm3d2_005_khg_f.anm"].time = maid0.body0.m_Bones.GetComponent<Animation>()["dance_cm3d2_005_khg_f.anm"].time;
                                 maid.body0.m_Bones.GetComponent<Animation>()["dance_cm3d2_005_khg_f.anm"].time = -0.400f;
                                 motionSetting[i] = true;
-//                                setCount++;
                             }
                         }
                     }
                     else{
                         motionSetting[i] = true;
-//                        setCount++;
                     }
                 }
             }
-//            if(setCount == MAX_LISTED_MAID){
-//                // 1秒後に同期合わせすることにする
-//                nextFire = Time.time + 1.0f;
-//            }
         }
         
         //------------------------------------------------------xml--------------------------------------------------------------------
@@ -132,6 +110,7 @@ namespace CM3D2.dance_cm3d2_006_ssn
             private XmlDocument xmldoc = new XmlDocument();
             public string[] listPreset = new string[MAX_LISTED_MAID];
             public Vector3[] listPos = new Vector3[MAX_LISTED_MAID];
+            public float fov;
             
             public XmlManager()
             {
@@ -160,6 +139,8 @@ namespace CM3D2.dance_cm3d2_006_ssn
                     listPos[maidNo].y =float.Parse(((XmlElement)presetFile).GetAttribute("Y"));
                     listPos[maidNo].z =float.Parse(((XmlElement)presetFile).GetAttribute("Z"));
                 }
+                presetList = xmldoc.GetElementsByTagName("Camera");
+                fov =float.Parse(((XmlElement)presetList[0]).GetAttribute("fieldOfView"));
             }
         }
 
